122293b8d36da22465537cbdced18ea3e2f21a9c
lersamart24 <sasawatgin.1@gmail.com>
2026-02-20 07:21:35 +0700

    Create  Mini Shop project.py

---
+++ Mini Shop project.py
@@ -0,0 +1,15 @@
+a = input("what your name: ")
+b = input("what your birthday (DD/MM/YYYY): ")
+print("your name is", a, "Your birthday is", b)
+
+mom = int(input("How much did mom give you? "))
+dad = int(input("How much did dad give you? "))
+total = mom + dad
+print("Total money:", total)
+
+pen = int(input("How many pens? "))
+pencil = int(input("How many pencils? "))
+price_pen = pen * 20
+price_pencil = pencil * 100
+total = price_pen + price_pencil
+print("Pen =", price_pen, "Pencil =", price_pencil, "Total =", total)
