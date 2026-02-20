b09635fe3b9972e70c44fee0802713f5c47d765e
lersamart24 <sasawatgin.1@gmail.com>
2026-02-20 13:39:44 +0700

    Create List and test 20_02_2026.py
    Signed-off-by: lersamart24 <sasawatgin.1@gmail.com>

---
+++ List and test 20_02_2026.py
@@ -0,0 +1,101 @@
+# #list
+# name = ["Gin","Tim","Tom","Timo","sup"]
+# print(name)
+# name.pop(0)
+
+# name.remove("sup")
+# print(name)
+
+# name.append("ginger")
+# print(name)
+
+# name.extend(["bob","note"])
+# print(name)
+
+# name.insert(2, "butter")
+# print(name)
+
+
+# name.clear()
+# print(fruit)
+
+#Dictionaries
+
+# grade = {"Tim":"A+", "note":"A"}
+# print(grade["note"])
+
+# grade["Timo"] = "B"
+# print(grade)
+
+
+# grade.update({"timo": "A+", "tom":"C+"})
+# print(grade)
+
+# del grade["timo"]
+# print(grade)
+
+
+# stuff = ["moon cake","toy"]
+# print(stuff)
+
+
+# stuff.append("rice")
+# print(stuff)
+
+
+# stuff.remove("toy")
+# print("i dont need anymore toy so this is my final list",stuff)
+
+# stuff.extend(["pork","fish"])
+# print(stuff)
+
+
+# stuff.pop(0)
+# print(stuff)
+
+
+# stuff.insert(2,"jellyfish")
+# print(stuff)
+
+
+# stuff.clear()
+# print("After we got food at home",stuff)
+
+
+shopping = ["mike","bread","egg"]
+print(shopping)
+
+
+shopping.append("pork")
+print(shopping)
+
+
+shopping.extend(["fish","rice"])
+print(shopping)
+
+shopping.insert(1, "water")
+print(shopping)
+
+shopping.remove("bread")
+print(shopping)
+
+
+shopping.pop(5)
+print(shopping)
+
+del shopping[0]
+print(shopping)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
