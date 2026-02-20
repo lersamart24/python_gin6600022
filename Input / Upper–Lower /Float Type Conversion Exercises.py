d0415f941ed51dfebdff55c4c44cdbdf1baf278e
lersamart24 <sasawatgin.1@gmail.com>
2026-02-20 07:19:34 +0700

    Create Float Type Conversion Exercises.py

---
+++ Input / Upper–Lower /Float Type Conversion Exercises.py
@@ -0,0 +1,10 @@
+# Kelvin → Fahrenheit
+kalvin = input("Can you enter your kalvin: ")
+kalvin_float = float(kalvin)
+fahrenheit = 1.8 * (kalvin_float - 273.15) + 32.0
+print("Your temperature is", fahrenheit, "fahrenheit")
+
+# Age +10
+age = input("Can you enter your age: ")
+age_10 = int(age) + 10
+print("Your age is", age_10, "years")
