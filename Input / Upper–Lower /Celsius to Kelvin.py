ea31f76a31ed2d8dbf3527fc49fe7476b3c7cd1e
lersamart24 <sasawatgin.1@gmail.com>
2026-02-20 07:18:40 +0700

    Create  Celsius to Kelvin.py

---
+++ Input / Upper–Lower /Celsius to Kelvin.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+print("Hello", name.upper())
+print("Nice to meet you", name.lower())
+
+celsius = input("Enter temperature in Celsius: ")
+kelvin = float(celsius) + 273.15
+print("Temperature in Kelvin:", kelvin)
