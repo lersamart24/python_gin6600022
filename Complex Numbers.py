df10113e4f600e7967c9354e36887afdba6d20af
lersamart24 <sasawatgin.1@gmail.com>
2026-02-20 07:20:42 +0700

    Create Complex Numbers.py

---
+++ Complex Numbers.py
@@ -0,0 +1,16 @@
+num1 = input("input your complex number: ")
+num2 = complex(num1)
+print("your complex number is", num2)
+
+comp1 = 5+5j
+comp2 = 6+7j
+total = comp1 + comp2
+print(total)
+
+y = complex(25, 2)
+x = 10 + 9j
+z = str(x) + str(y)
+print(z)
+print(type(y))
+print(type(z))
+print(type(x))
