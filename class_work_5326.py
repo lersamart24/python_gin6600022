c28e010b073bf14f3d3b63cc1ca086e92bbe77e7
lersamart24 <sasawatgin.1@gmail.com>
2026-03-06 12:19:36 +0700

    Create class_work_5326.py
    Signed-off-by: lersamart24 <sasawatgin.1@gmail.com>

---
+++ class_work_5326.py
@@ -0,0 +1,264 @@
+# score = int(input("enter your test score"))
+# if score > 50:
+#     print("pass")
+
+# else:
+#    print("fail ") 
+
+# number = int(input("enter a number"))
+# if number % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+
+
+# number = int(input("enter a number"))
+# while number>=0:
+#     print(number)
+#     number -= 1
+# print("start")
+
+# number = int(input("enter a number"))
+# for g in range(1, number +1):
+#     print(g)
+
+
+# food_lists = []
+
+# for i in range(3):
+#     food_list = input("what food you like")
+#     food_lists.append(food_list)
+# print(food_lists)
+
+
+
+# shopping_list = []
+
+# for h in range(5):
+#     food_list = input("what food you like")
+#     shopping_list.append(food_list)
+
+# remove = input("do you want to remove the first list")
+# if remove == "yes":
+#     shopping_list.pop(0)
+# else:
+#     print("ok")
+    
+# food = input("do you want to remove anything")
+# if food in shopping_list:
+#     shopping_list.remove(food)
+#     print(shopping_list)
+
+# else:
+#     print("item not found")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
