7b2f867feeee25a9458964e6c7b696c3be8b4127
lersamart24 <sasawatgin.1@gmail.com>
2026-03-27 12:26:22 +0700

    Create List_Slice.py
    Signed-off-by: lersamart24 <sasawatgin.1@gmail.com>

---
+++ List_Slice.py
@@ -0,0 +1,79 @@
+# fruit_list = ["apple","banana","apple","orange"]
+# fruit_list.append("mango")
+# fruit_list.extend(["grape","melon"])
+# fruit_list.remove("apple")
+# print(fruit_list)
+# fruit_list.reverse()
+# print(fruit_list)
+
+
+# items = ["mik","egg","bread","egg","juice"]
+# items.append("butter")
+# items.extend(["cheese","ham"])
+# items.remove("egg")
+# print(items.count("egg"))
+# items.pop(4)
+# items.reverse()
+# print(items)
+
+
+# animal_list = ["animals","cat","dog","rabbit"]
+# animal_list.append("tiger")
+# animal_list.extend(["lion","elephant"])
+# animal_list.remove("dog")
+# print(animal_list.count("dog"))
+# print(animal_list)
+# animal_list.pop(5)
+# animal_list.reverse()
+# print(animal_list)
+
+
+# nums = [10,20,10,30,40]
+# nums.append(50)
+# nums.extend([60,70])
+# nums.remove(10)
+# print(nums.count(10))
+# print(nums)
+# nums.pop(6)
+# nums.reverse()
+# print(nums)
+
+
+
+
+# inventory = ["sword","shield","potion","potion"]
+# inventory.append("amor")
+# inventory.extend(["ring","boots"])
+# print(inventory.count("potion"))
+# print(inventory)
+# inventory.pop(6)
+# inventory.reverse()
+# print(inventory)
+
+
+
+# nums = ["1","2","3","4","5","6","7"]
+# wh
+# print(nums[-4:5])
+
+
+
+
+
+# crayon_shop = ["red","blue","purple"]
+# print(crayon_shop)
+# while True:
+#     crayon = input("what crayon color do you want to get")
+#     crayon_shop.append(crayon)
+#     AI_crayon_prediction = crayon_shop[-5:]
+#     print(f"Your fav crayon list: {AI_crayon_prediction}")
+#     red = AI_crayon_prediction.count("red")
+#     blue = AI_crayon_prediction.count("blue")
+#     if red > blue:
+#         print("crayon_predicted you pick red")
+#     elif blue > red:
+#         print("crayon_predicted you pick blue")
+   
+#     else:
+#         print("you don't know what to pick/there no color that you pick")
+    
