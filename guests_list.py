c6c329a658ed5755e7a1f4c4ed109767b1691729
lersamart24 <sasawatgin.1@gmail.com>
2026-04-21 16:08:51 +0700

    Create guests_list.py
    Signed-off-by: lersamart24 <sasawatgin.1@gmail.com>

---
+++ guests_list.py
@@ -0,0 +1,29 @@
+# guests = ["Alice","Bob","Charlie"]
+# guests.insert(0, "Daisy")
+# guests.append("Ethan")
+# guests.extend(["Fiona","George"])
+# guests.remove("Bob")
+# print(guests)
+# lastguest = guests.pop(5)
+# print(lastguest)
+# print(guests)
+
+
+
+
+
+
+
+
+# filtered_number = []
+
+# for i in range(1, 21):
+#     if i % 3 == 0:
+#         print("it divisible by 3", i)
+
+#     elif i % 2 == 0:
+#         continue
+#     else:
+#         filtered_number.append(i)
+
+# print(filtered_number)
