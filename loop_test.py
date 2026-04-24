bc287d179e10f135bf61627cafa2de5882b86572
lersamart24 <sasawatgin.1@gmail.com>
2026-04-24 11:55:43 +0700

    Create loop_test.py
    Signed-off-by: lersamart24 <sasawatgin.1@gmail.com>

---
+++ loop_test.py
@@ -0,0 +1,24 @@
+# for i in range(20):
+#     if i >= 10:
+#         print(i)
+
+# for i in range(1, 25, 3):
+#     print(i)
+
+
+# for i in range(10, 0, -1):
+#     print(i)
+    
+# print("Blast off!")
+
+
+# for i in range(1, 16, 2):
+#     print(i)
+    
+
+# sum = 0
+# for i in range(5, 16):
+#     print(i)
+#     sum = sum + i
+# print("-------------------")
+# print(sum)
