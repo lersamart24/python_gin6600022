4c5874e00cfe901089c83bfb5e3c2a886a347647
lersamart24 <sasawatgin.1@gmail.com>
2026-05-08 12:32:04 +0700

    Create Dlct_cool_stuff.py
    Signed-off-by: lersamart24 <sasawatgin.1@gmail.com>

---
+++ Dlct_cool_stuff.py
@@ -0,0 +1,11 @@
+backpack = {"weapon": ["sword","potion"]}
+
+selling = input("what do you want buy(sword/potion)")
+if selling == "sword":
+    print("you ask for the sword and  get it")
+
+elif selling == "potion":
+    print("you ask for the sword and get it")
+
+else:
+    print("i don't have that in store")
